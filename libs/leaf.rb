3072462b0b1bafecf378f8f4e7e4c6be0b88ca4b
Tim Morgan <riscfuture@gmail.com>
2008-07-16 15:01:45 -0700

    Now chooses the correct view for aliased commands that return strings

--- libs/leaf.rb
+++ libs/leaf.rb
@@ -638,10 +638,12 @@ module Autumn
       return_val = send(cmd_sym, stem, sender, reply_to, msg)
       view = Thread.current[:render_view]
       view ||= @@view_alias[name]
-      if options[:views][view.to_s] then
+      if return_val.kind_of? String then
+        stem.message return_val, reply_to
+      elsif options[:views][view.to_s] then
         stem.message parse_view(view.to_s), reply_to
       else
-        stem.message return_val, reply_to
+        raise "You must either specify a view to render or return a string to send."
       end
       Thread.current[:vars] = nil
       Thread.current[:render_view] = nil # Clear it out in case the command is synchronized
