64fda32795e4ddd7ec4b80a201769916cf436456
Tim Morgan <riscfuture@gmail.com>
2009-06-12 02:47:40 -0700

    Proper timestamp usage

--- leaves/scorekeeper/lib/datamapper_extensions.rb
+++ leaves/scorekeeper/lib/datamapper_extensions.rb
@@ -1,1 +1,20 @@
+require 'dm-validations'
 require 'dm-timestamps'
+
+DataMapper::Timestamp.class_eval do
+  private
+  
+  #HACK Use the current repository, not the default one.
+  #
+  # Updates this method to use the currently-scoped repository rather than the
+  # model's default repository.
+  
+  def set_timestamps
+    return unless dirty? || new_record?
+    TIMESTAMP_PROPERTIES.each do |name,(_type,proc)|
+      if model.properties(repository.name).has_property?(name)
+        model.properties(repository.name)[name].set(self, proc.call(self, model.properties(repository.name)[name])) unless attribute_dirty?(name)
+      end
+    end
+  end
+end
