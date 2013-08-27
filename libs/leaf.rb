RISCfuture/autumn

commit ee38e8c5d7764beb2f8fcc3b2824c655cbcb6daa

* Manual style changes * Platform independence

libs/leaf.rb
@@ -181,7 +181,7 @@ module Autumn
       # Let the stems array respond to methods as if it were a single stem
       class << @stems
         def method_missing(meth, *args)
-          if all? { |stem| stem.respond_to? meth } then
+          if all? { |stem| stem.respond_to? meth }
             collect { |stem| stem.send(meth, *args) }
           else
             super
@@ -191,7 +191,7 @@ module Autumn
     end
 
     def preconfigure # :nodoc:
-      if options[:authentication] then
+      if options[:authentication]
         @authenticator = Autumn::Authentication.const_get(options[:authentication]['type'].camelcase(:upper)).new(options[:authentication].rekey(&:to_sym))
         stems.add_listener @authenticator
       end
@@ -200,7 +200,7 @@ module Autumn
     # Simplifies method calls for one-stem leaves.
 
     def method_missing(meth, *args) # :nodoc:
-      if stems.size == 1 and stems.only.respond_to? meth then
+      if stems.size == 1 && stems.only.respond_to?(meth)
         stems.only.send meth, *args
       else
         super
@@ -209,14 +209,14 @@ module Autumn
 
     ########################## METHODS INVOKED BY STEM #########################
 
-    def stem_ready(stem) # :nodoc:
-      return unless Thread.exclusive { stems.ready?.all? }
+    def stem_ready(_) # :nodoc:
+      return unless (@ready_mutex ||= Mutex.new).synchronize { stems.ready?.all? }
       database { startup_check }
     end
 
     def irc_privmsg_event(stem, sender, arguments) # :nodoc:
       database do
-        if arguments[:channel] then
+        if arguments[:channel]
           command_parse stem, sender, arguments
           did_receive_channel_message stem, sender, arguments[:channel], arguments[:message]
         else
@@ -236,10 +236,10 @@ module Autumn
 
     def irc_mode_event(stem, sender, arguments) # :nodoc:
       database do
-        if arguments[:recipient] then
+        if arguments[:recipient]
           gained_usermodes(stem, arguments[:mode]) { |prop| someone_did_gain_usermode stem, arguments[:recipient], prop, arguments[:parameter], sender }
           lost_usermodes(stem, arguments[:mode]) { |prop| someone_did_lose_usermode stem, arguments[:recipient], prop, arguments[:parameter], sender }
-        elsif arguments[:parameter] and stem.server_type.privilege_mode?(arguments[:mode]) then
+        elsif arguments[:parameter] && stem.server_type.privilege_mode?(arguments[:mode])
           gained_privileges(stem, arguments[:mode]) { |prop| someone_did_gain_privilege stem, arguments[:channel], arguments[:parameter], prop, sender }
           lost_privileges(stem, arguments[:mode]) { |prop| someone_did_lose_privilege stem, arguments[:channel], arguments[:parameter], prop, sender }
         else
@@ -263,7 +263,7 @@ module Autumn
 
     def irc_notice_event(stem, sender, arguments) # :nodoc:
       database do
-        if arguments[:recipient] then
+        if arguments[:recipient]
           did_receive_notice stem, sender, arguments[:recipient], arguments[:message]
         else
           did_receive_notice stem, sender, arguments[:channel], arguments[:message]
@@ -311,7 +311,7 @@ module Autumn
 
     def database(dbname=nil, &block)
       dbname ||= database_name
-      if dbname then
+      if dbname
         repository dbname, &block
       else
         yield
@@ -323,8 +323,8 @@ module Autumn
     # or this leaf's class name. Returns nil if no suitable connection is found.
 
     def database_name # :nodoc:
-      return nil unless Module.constants.include? 'DataMapper' or Module.constants.include? :DataMapper
-      raise "No such database connection #{options[:database]}" if options[:database] and DataMapper::Repository.adapters[options[:database]].nil?
+      return nil unless Module.constants.include?('DataMapper') || Module.constants.include?(:DataMapper)
+      raise "No such database connection #{options[:database]}" if options[:database] && DataMapper::Repository.adapters[options[:database]].nil?
       # Custom database connection specified
       return options[:database].to_sym if options[:database]
       # Leaf config name
@@ -393,10 +393,10 @@ module Autumn
     # and if the result is not false or nil, the command will be executed.
 
     def self.before_filter(filter, options={})
-      if options[:only] and not options[:only].kind_of? Array then
+      if options[:only] && !options[:only].kind_of?(Array)
         options[:only] = [options[:only]]
       end
-      if options[:except] and not options[:except].kind_of? Array then
+      if options[:except] && !options[:except].kind_of?(Array)
         options[:except] = [options[:except]]
       end
       write_inheritable_array 'before_filters', [[filter.to_sym, options]]
@@ -420,10 +420,10 @@ module Autumn
     #  clean_tmp_filter <stem>, <channel>, <sender hash>, :sendfile, <message>, { remove_symlinks: true }
 
     def self.after_filter(filter, options={})
-      if options[:only] and not options[:only].kind_of? Array then
+      if options[:only] && !options[:only].kind_of?(Array)
         options[:only] = [options[:only]]
       end
-      if options[:except] and not options[:except].kind_of? Array then
+      if options[:except] && !options[:except].kind_of?(Array)
         options[:except] = [options[:except]]
       end
       write_inheritable_array 'after_filters', [[filter.to_sym, options]]
@@ -546,7 +546,7 @@ module Autumn
     def someone_did_quit(stem, person, msg)
     end
 
-    UNADVERTISED_COMMANDS = ['about', 'commands'] # :nodoc:
+    UNADVERTISED_COMMANDS = %w(about commands) # :nodoc:
 
     # Typing this command displays a list of all commands for each leaf running
     # off this stem.
@@ -612,13 +612,12 @@ module Autumn
     end
 
     def command_parse(stem, sender, arguments)
-      if arguments[:channel] or options[:respond_to_private_messages] then
+      if arguments[:channel] || options[:respond_to_private_messages]
         reply_to = arguments[:channel] ? arguments[:channel] : sender[:nick]
         matches  = arguments[:message].match(/^#{Regexp.escape options[:command_prefix]}(\w+)\s*(.*)$/)
-        if matches then
+        if matches
           name   = matches[1].to_sym
           msg    = matches[2]
-          origin = sender.merge(stem: stem)
           command_exec name, stem, arguments[:channel], sender, msg, reply_to
         end
       end
@@ -627,7 +626,7 @@ module Autumn
     def command_exec(name, stem, channel, sender, msg, reply_to)
       cmd_sym = "#{name}_command".to_sym
       return unless respond_to? cmd_sym
-      msg = nil if msg.empty?
+      msg = nil if msg.presence
 
       return unless authenticated?(name, stem, channel, sender)
       return unless run_before_filters(name, stem, channel, sender, name, msg)
@@ -636,9 +635,9 @@ module Autumn
       return_val            = send(cmd_sym, stem, sender, reply_to, msg)
       view                  = Thread.current[:render_view]
       view                  ||= @@view_alias[name]
-      if return_val.kind_of? String then
+      if return_val.kind_of? String
         stem.message return_val, reply_to
-      elsif options[:views][view.to_s] then
+      elsif options[:views][view.to_s]
         stem.message parse_view(view.to_s), reply_to
       #else
       #  raise "You must either specify a view to render or return a string to send."
@@ -657,23 +656,23 @@ module Autumn
       Foliater.instance.leaves.key self
     end
 
-    def run_before_filters(cmd, stem, channel, sender, command, msg)
+    def run_before_filters(cmd, stem, channel, sender, _, msg)
       command = cmd.to_sym
       self.class.before_filters.each do |filter, options|
         local_opts = options.dup
-        next if local_opts[:only] and not local_opts.delete(:only).include? command
-        next if local_opts[:except] and local_opts.delete(:except).include? command
+        next if local_opts[:only] && !local_opts.delete(:only).include?(command)
+        next if local_opts[:except] && local_opts.delete(:except).include?(command)
         return false unless method("#{filter}_filter")[stem, channel, sender, command, msg, local_opts]
       end
       return true
     end
 
-    def run_after_filters(cmd, stem, channel, sender, command, msg)
+    def run_after_filters(cmd, stem, channel, sender, _, msg)
       command = cmd.to_sym
       self.class.after_filters.each do |filter, options|
         local_opts = options.dup
-        next if local_opts[:only] and not local_opts.delete(:only).include? command
-        next if local_opts[:except] and local_opts.delete(:except).include? command
+        next if local_opts[:only] && !local_opts.delete(:only).include?(command)
+        next if local_opts[:except] && local_opts.delete(:except).include?(command)
         method("#{filter}_filter")[stem, channel, sender, command, msg, local_opts]
       end
     end
@@ -681,10 +680,8 @@ module Autumn
     def authenticated?(cmd, stem, channel, sender)
       return true if @authenticator.nil?
       # Any method annotated as protected is authenticated unconditionally
-      if not self.class.ann("#{cmd}_command".to_sym, :protected) then
-        return true
-      end
-      if @authenticator.authenticate(stem, channel, sender, self) then
+      return true unless self.class.ann("#{cmd}_command".to_sym, :protected)
+      if @authenticator.authenticate(stem, channel, sender, self)
         return true
       else
         stem.message @authenticator.unauthorized, channel unless options[:authentication]['silent']
@@ -723,11 +720,11 @@ module Autumn
     end
 
     def self.before_filters
-      read_inheritable_attribute('before_filters') or []
+      read_inheritable_attribute('before_filters') || []
     end
 
     def self.after_filters
-      read_inheritable_attribute('after_filters') or []
+      read_inheritable_attribute('after_filters') || []
     end
   end
 end

Gemfile
@@ -18,7 +18,8 @@ group :datamapper do
 end
 
 # load each leaf's gem requirements in its own group
-Dir.glob("#{File.dirname __FILE__}/leaves/*/Gemfile").each do |gemfile|
+files = Pathname.new(__FILE__).dirname.join('leaves', '*', 'Gemfile')
+Pathname.glob(files).each do |gemfile|
   eval File.read(gemfile)
 end
 

README.rdoc
@@ -289,7 +289,7 @@ and in production, it contains:
 Now, in your code, you might have a method like:
 
  def scores_command(stem, sender, reply_to, msg)
-   if options[:dont_http] then
+   if options[:dont_http]
      return "Some fake sports scores."
    else
      # go on the web and find real sports scores

Rakefile
@@ -1,7 +1,7 @@
 require 'rake'
 $: << Dir.getwd
 require 'libs/autumn'
-$: << Autumn::Config.root unless Autumn::Config.root == Dir.getwd
+$: << Autumn::Config.root.to_s unless Autumn::Config.root.to_s == Dir.getwd
 require 'libs/genesis'
 
 task :default do
@@ -23,47 +23,47 @@ end
 namespace :app do
   desc "Launch the Autumn daemon"
   task :start do
-    system 'script/daemon start'
+    system 'script/daemon', 'start'
   end
 
   desc "Stop the Autumn daemon"
   task :stop do
-    system 'script/daemon stop'
+    system 'script/daemon', 'stop'
   end
 
   desc "Restart the Autumn daemon"
   task :restart do
-    system 'script/daemon restart'
+    system 'script/daemon', 'restart'
   end
 
   desc "Start Autumn but not as a daemon (stay on top)"
   task :run do
-    system 'script/daemon run'
+    system 'script/daemon', 'run'
   end
 
   desc "Force the daemon to a stopped state (clears PID files)"
   task :zap do
-    system 'script/daemon zap'
+    system 'script/daemon', 'zap'
   end
 end
 
 namespace :log do
   desc "Remove all log files"
   task :clear do
-    system 'rm -vf tmp/*.log tmp/*.output log/*.log*'
+    system 'rm', '-vf', 'tmp/*.log', 'tmp/*.output', 'log/*.log*'
   end
 
   desc "Print all error messages in the log files"
   task errors: :environment do
-    season_log = "log/#{@genesis.config.global :season}.log"
-    system_log = 'tmp/autumn.log'
-    if File.exists? season_log then
+    season_log = Pathname.new('log').join(@genesis.config.global(:season), 'log')
+    system_log = Pathname.new('tmp').join('autumn.log')
+    if season_log.exist?
       puts "==== ERROR-LEVEL LOG MESSAGES ===="
       File.open(season_log, 'r') do |log|
         puts log.grep(/^[EF],/)
       end
     end
-    if File.exists? system_log then
+    if system_log.exist?
       puts "====   UNCAUGHT EXCEPTIONS    ===="
       File.open(system_log, 'r') do |log|
         puts log.grep(/^[EF],/)
@@ -73,7 +73,7 @@ namespace :log do
 end
 
 def local_db?(db)
-  db.host.nil? or db.host == 'localhost'
+  db.host.nil? || db.host == 'localhost'
 end
 
 namespace :db do
@@ -104,32 +104,46 @@ end
 namespace :doc do
   desc "Generate API documentation for Autumn"
   task api: :environment do
-    FileUtils.remove_dir 'doc/api' if File.directory? 'doc/api'
-    system "rdoc --main README.rdoc --title 'Autumn API Documentation' -o doc/api libs README.rdoc"
+    api_doc = Pathname.new('doc').join('api')
+    FileUtils.remove_dir api_doc if api_doc.directory?
+    system 'rdoc',
+           '--main', 'README.rdoc',
+           '--title', "Autumn API Documentation",
+           '-o', api_doc,
+           'libs', 'README.rdoc'
   end
 
   desc "Generate documentation for all leaves"
   task leaves: :environment do
-    FileUtils.remove_dir 'doc/leaves' if File.directory? 'doc/leaves'
-    Dir.glob("leaves/*").each do |leaf_dir|
+    leaves_doc = Pathname.new('doc').join('leaves')
+    FileUtils.remove_dir leaves_doc if leaves_doc.directory?
+    Pathname.new('leaves').glob('*').each do |leaf_dir|
       Dir.chdir leaf_dir do
-        system "rdoc --main README.rdoc --title '#{File.basename(leaf_dir).camelcase(:upper)} Documentation' -o ../../doc/leaves/#{File.basename leaf_dir} --line-numbers --inline-source controller.rb helpers models README"
+        system 'rdoc',
+                   '--main', 'README.rdoc',
+                   '--title', "#{leaf_dir.basename.camelcase(:upper)} Documentation",
+                   '-o', Pathname.new(__FILE__).basename.join('doc', 'leaves', leaf_dir.basename).to_s,
+                   '--line-numbers',
+                   '--inline-source',
+                   'controller.rb', 'helpers', 'models', 'README'
       end
     end
   end
 
   desc "Remove all documentation"
   task clear: :environment do
-    FileUtils.remove_dir 'doc/api' if File.directory? 'doc/api'
-    FileUtils.remove_dir 'doc/leaves' if File.directory? 'doc/leaves'
+    api_doc = Pathname.new('doc').join('api')
+    leaves_doc = Pathname.new('doc').join('leaves')
+    FileUtils.remove_dir api_doc if api_doc.directory?
+    FileUtils.remove_dir leaves_doc if leaves_doc.directory?
   end
 end
 
 # Load any custom Rake tasks in the bot's tasks directory.
-Dir["leaves/*"].each do |leaf|
-  leaf_name = File.basename(leaf, ".rb").downcase
+Pathname.new('leaves').glob('*').each do |leaf|
+  leaf_name = leaf.basename('.rb').downcase
   namespace leaf_name.to_sym do # Tasks are placed in a namespace named after the leaf
-    FileList["leaves/#{leaf_name}/tasks/**/*.rake"].sort.each do |task|
+    Pathname.glob(leaf.join('tasks', '**', '*.rake')).sort.each do |task|
       load task
     end
   end

leaves/administrator/controller.rb
@@ -11,10 +11,10 @@ class Controller < Autumn::Leaf
   # This command does not reload the YAML configuration files, only the source
   # code.
 
-  def reload_command(stem, sender, reply_to, msg)
+  def reload_command(_, _, _, msg)
     var leaves: Hash.new
     if msg then
-      if Foliater.instance.leaves.include?(msg) then
+      if Foliater.instance.leaves.include?(msg)
         begin
           Foliater.instance.hot_reload Foliater.instance.leaves[msg]
         rescue
@@ -47,7 +47,7 @@ class Controller < Autumn::Leaf
 
   # Typing this command will cause the Stem to exit.
 
-  def quit_command(stem, sender, reply_to, msg)
+  def quit_command(stem, _, _, _)
     stem.quit
   end
   ann :quit_command, protected: true
@@ -55,13 +55,13 @@ class Controller < Autumn::Leaf
   # Typing this command will display information about the version of Autumn
   # that is running this leaf.
 
-  def autumn_command(stem, sender, reply_to, msg)
+  def autumn_command(_, _, _, _)
     var version: Autumn::Config.version
   end
 
   # Suppress the !commands command; don't want to publicize the administrative
   # features.
 
-  def commands_command(stem, sender, reply_to, msg)
+  def commands_command(_, _, _, _)
   end
 end

leaves/insulter/controller.rb
@@ -5,13 +5,13 @@ class Controller < Autumn::Leaf
 
   # Insults the unfortunate argument of this command.
 
-  def insult_command(stem, sender, reply_to, msg)
+  def insult_command(_, _, _, msg)
     msg.nil? ? render(:help) : insult(msg.capitalize)
   end
 
   # Displays information about the leaf.
 
-  def about_command(stem, sender, reply_to, msg)
+  def about_command(_, _, _, _)
   end
 
   private
@@ -29,31 +29,26 @@ class Controller < Autumn::Leaf
       'a warped', 'a wayward', 'a weedy', 'a yeasty'
   ]
 
-  PARTICIPLES = [
-      'base-court', 'bat-fowling', 'beef-witted', 'beetle-headed', 'boil-brained',
-      'clapper-clawed', 'clay-brained', 'common-kissing', 'crook-pated',
-      'dismal-dreaming', 'dizzy-eyed', 'doghearted', 'dread-bolted',
-      'earth-vexing', 'elf-skinned', 'fat-kidneyed', 'fen-sucked', 'flap-mouthed',
-      'fly-bitten', 'folly-fallen', 'fool-born', 'full-gorged', 'guts-griping',
-      'half-faced', 'hasty-witted', 'hedge-born', 'hell-hated', 'idle-headed',
-      'ill-breeding', 'ill-nurtured', 'knotty-pated', 'milk-livered',
-      'motley-minded', 'onion-eyed', 'plume-plucked', 'pottle-deep', 'pox-marked',
-      'reeling-ripe', 'rough-hewn', 'rude-growing', 'rump-fed', 'shard-borne',
-      'sheep-biting', 'spur-galled', 'swag-bellied', 'tardy-gaited',
-      'tickle-brained', 'toad-spotted', 'urchin-snouted', 'weather-bitten'
-  ]
+  PARTICIPLES = %w(base-court bat-fowling beef-witted beetle-headed boil-brained
+                   clapper-clawed clay-brained common-kissing crook-pated
+                   dismal-dreaming dizzy-eyed doghearted dread-bolted
+                   earth-vexing elf-skinned fat-kidneyed fen-sucked flap-mouthed
+                   fly-bitten folly-fallen fool-born full-gorged guts-griping
+                   half-faced hasty-witted hedge-born hell-hated idle-headed
+                   ill-breeding ill-nurtured knotty-pated milk-livered
+                   motley-minded onion-eyed plume-plucked pottle-deep pox-marked
+                   reeling-ripe rough-hewn rude-growing rump-fed shard-borne
+                   sheep-biting spur-galled swag-bellied tardy-gaited
+                   tickle-brained toad-spotted urchin-snouted weather-bitten)
 
-  NOUNS = [
-      'apple-john', 'baggage', 'barnacle', 'bladder', 'boar-pig', 'bugbear',
-      'bum-bailey', 'canker-blossom', 'clack-dish', 'clotpole', 'codpiece',
-      'coxcomb', 'death-token', 'dewberry', 'flap-dragon', 'flax-wench',
-      'flirt-gill', 'foot-licker', 'fustilarian', 'giglet', 'gudgeon', 'haggard',
-      'harpy', 'hedge-pig', 'horn-beast', 'hugger-mugger', 'joithead', 'lewdster',
-      'lout', 'maggot-pie', 'malt-worm', 'mammet', 'measle', 'minnow',
-      'miscreant', 'moldwarp', 'mumble-news', 'nut-hook', 'pigeon-egg', 'pignut',
-      'pumpion', 'puttock', 'ratsbane', 'scut', 'skainsmate', 'strumpet',
-      'varlet', 'vassal', 'wagtail', 'whey-face'
-  ]
+  NOUNS = %w(apple-john baggage barnacle bladder boar-pig bugbear bum-bailey
+             canker-blossom clack-dish clotpole codpiece coxcomb death-token
+             dewberry flap-dragon flax-wench flirt-gill foot-licker fustilarian
+             giglet gudgeon haggard harpy hedge-pig horn-beast hugger-mugger
+             joithead lewdster lout maggot-pie malt-worm mammet measle minnow
+             miscreant moldwarp mumble-news nut-hook pigeon-egg pignut pumpion
+             puttock ratsbane scut skainsmate strumpet varlet vassal wagtail
+             whey-face)
 
   def insult(victim)
     var adjective: ADJECTIVES.sample

leaves/scorekeeper/controller.rb
@@ -6,19 +6,19 @@ class Controller < Autumn::Leaf
 
   # Displays an about message.
 
-  def about_command(stem, sender, reply_to, msg)
+  def about_command(_, _, _, _)
   end
 
   # Displays the current point totals, or modifies someone's score, depending on
   # the message provided with the command.
 
   def points_command(stem, sender, reply_to, msg)
-    if msg.nil? or msg.empty? then
+    if msg.blank?
       var totals: totals(stem, reply_to)
-    elsif msg =~ /^(#{stem.nick_regex})\s+history\s*(.*)$/ then
+    elsif msg =~ /^(#{stem.nick_regex})\s+history\s*(.*)$/
       parse_history stem, reply_to, $1, $2
       render :history
-    elsif msg =~ /^(#{nick_regex})\s+([\+\-]\d+)\s*(.*)$/ then
+    elsif msg =~ /^(#{nick_regex})\s+([\+\-]\d+)\s*(.*)$/
       parse_change stem, reply_to, sender, $1, $2.to_i, $3
       render :change
     else
@@ -40,12 +40,12 @@ class Controller < Autumn::Leaf
 
   def parse_change(stem, channel, sender, victim, delta, note)
     giver = find_person(stem, sender[:nick])
-    if giver.nil? and options[:scoring] == 'open' then
+    if giver.nil? && options[:scoring] == 'open'
       giver ||= Person.create(server: server_identifier(stem), name: sender[:nick])
     end
 
     receiver = find_person(stem, victim)
-    if receiver.nil? and options[:scoring] == 'open' then
+    if receiver.nil? && options[:scoring] == 'open'
       receiver ||= Person.create(server: server_identifier(stem), name: find_in_channel(stem, channel, victim))
     end
 
@@ -63,24 +63,24 @@ class Controller < Autumn::Leaf
 
   def parse_history(stem, channel, subject, argument)
     date   = argument.empty? ? nil : parse_date(argument)
-    scores = Array.new
 
     chan   = Channel.named(channel).first
     person = find_person(stem, subject)
-    if person.nil? then
+    if person.nil?
       var person: subject
       var no_history: true
       return
     end
 
-    if date then
+    giver = nil
+    if date
       start, stop = find_range(date)
       scores      = chan.scores.given_to(person).between(start, stop).newest_first.all(limit: 5)
-    elsif argument.empty? then
+    elsif argument.empty?
       scores = chan.scores.given_to(person).newest_first.all(limit: 5)
     else
       giver = find_person(stem, argument)
-      if giver.nil? then
+      if giver.nil?
         var giver: argument
         var receiver: person
         var no_giver_history: true

leaves/scorekeeper/helpers/general.rb
@@ -9,18 +9,17 @@ module GeneralHelper
       begin
         date = Date.parse(str)
       rescue ArgumentError
+        # ignored
       end
     end
     return date
   end
 
   def find_range(date)
-    start = nil
-    stop  = nil
-    if date.kind_of? Range then
+    if date.kind_of? Range
       start = date.first
       stop  = date.last
-    elsif date.kind_of? Time then
+    elsif date.kind_of? Time
       start = date.to_date
       stop  = date.to_date + 1
     else
@@ -32,13 +31,13 @@ module GeneralHelper
 
   def find_person(stem, nick)
     Person.all(server: server_identifier(stem)).each do |person|
-      return person if person.name.downcase == normalize(nick) or person.pseudonyms.collect { |pn| pn.name.downcase }.include? normalize(nick)
+      return person if person.name.downcase == normalize(nick) || person.pseudonyms.collect { |pn| pn.name.downcase }.include?(normalize(nick))
     end
     return nil
   end
 
   def find_in_channel(stem, channel, victim)
-    stem.channel_members[channel].each do |name, privilege|
+    stem.channel_members[channel].each do |name, _|
       return normalize(name, false) if normalize(name) == normalize(victim)
     end
     return victim
@@ -49,7 +48,7 @@ module GeneralHelper
   end
 
   def authorized?(giver, receiver)
-    giver and receiver and giver.authorized? and giver.name != receiver.name
+    giver && receiver && giver.authorized? && giver.name != receiver.name
   end
 
   def change_points(stem, channel, giver, receiver, delta, note=nil)

commit 02c559cdcb986dd98482bd7be4ffbb6b9ab75a58

Fixes bug where no leaf controllers recieve messages Bug was caused in commit ee38e8c5 (Manual style changes)
Message would be set as nil when there was a message, as
"stuff".presence => "stuff". Likewise if there was no message,
msg would be kept as "" as "".presence => nil.

libs/leaf.rb
@@ -780,7 +780,7 @@ module Autumn
     def command_exec(name, stem, channel, sender, msg, reply_to)
       cmd_sym = "#{name}_command".to_sym
       return unless respond_to? cmd_sym
-      msg = nil if msg.presence
+      msg = msg.presence
 
       return unless authenticated?(name, stem, channel, sender)
       return unless run_before_filters(name, stem, channel, sender, name, msg)